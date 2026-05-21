02bc9d285c925d9a19360898917fa39441b78034
nprocakprocak <nprocak@gmail.com>
2026-05-21 00:00:05 -0300

    fix: animation when extracting from stack

--- apps/backend/src/routes/stacks.ts
+++ apps/backend/src/routes/stacks.ts
@@ -149,7 +149,7 @@ const extractItemFromStack: RequestHandler = async (req, res) => {
   }
   
   const item = await takeItemFromStack(id, emptyPosition);
-  res.status(200).json(item);
+  res.status(200).json({ ...item, fromX: existingStack.x, fromY: existingStack.y });
 }
 
 stacksRouter.get("/", listStacks);
