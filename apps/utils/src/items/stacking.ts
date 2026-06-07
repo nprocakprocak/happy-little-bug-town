9cf3680c5c2e5fddedb2fc6b474dd0d8b9de5b1a
nprocakprocak <nprocak@gmail.com>
2026-06-07 17:53:31 -0300

    chore: leaf rake now allows stacking other items

--- apps/utils/src/items/stacking.ts
+++ apps/utils/src/items/stacking.ts
@@ -4,15 +4,21 @@ import { isToolCrafted, ToolForCraft } from "../tools/craft.js";
 
 const LEAF_RAKE_TOOL_TYPE: ToolType = "leaf_rake";
 
+const LEAF_RAKE_STACKABLE_ITEM_TYPES: ReadonlySet<ItemType> = new Set([
+  "leaf_part",
+  "stick",
+  "root",
+]);
+
 export function hasCraftedTool(tools: ToolForCraft[], toolType: ToolType): boolean {
   const tool = tools.find((candidate) => candidate.toolType === toolType);
   return tool !== undefined && isToolCrafted(tool);
 }
 
 export function canStackItemType(itemType: ItemType, tools: ToolForCraft[]): boolean {
-  if (itemType === "leaf_part") {
-    return hasCraftedTool(tools, LEAF_RAKE_TOOL_TYPE);
+  if (!LEAF_RAKE_STACKABLE_ITEM_TYPES.has(itemType)) {
+    return false;
   }
 
-  return true;
+  return hasCraftedTool(tools, LEAF_RAKE_TOOL_TYPE);
 }
